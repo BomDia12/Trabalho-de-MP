6c0c646818c539a8eae7fccd656ede2c95882285
BomDia12 <chamatico2@gmail.com>
2021-10-04 16:56:45 -0300

    :test_tube: Adding tests
    Adding tests for user model and validations

--- Api/spec/models/user_spec.rb
+++ Api/spec/models/user_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { build :user }
+
   describe 'factory' do
     context 'when using standard user factory' do
-      it { expect(build(:user)).to be_valid }
+      it { expect(user).to be_valid }
+    end
+  end
+
+  describe 'validations' do
+    context 'user has no name' do
+      it { expect(build(:user, name: nil)).to be_invalid }
+    end
+
+    context 'user has too short of a name' do
+      it { expect(build(:user, name: '')).to be_invalid }
+    end
+
+    context 'user has no email' do
+      it { expect(build(:user, email: nil)).to be_invalid }
+    end
+
+    context 'user has a repeated email' do
+      it { expect(build(:user, email: user.email)).to be_invalid }
     end
   end
 end
