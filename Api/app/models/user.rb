76295bb7a21e252dc9a8f32dd2bf62c49ccffe09
BomDia12 <chamatico2@gmail.com>
2021-10-04 16:12:39 -0300

    :passport_control: Adding User authentication
    Adding simple token authentication gem, and setting the ambient so that token authentication is enabled

--- Api/app/models/user.rb
+++ Api/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # hook for easy token authentication (H2)
+  acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
