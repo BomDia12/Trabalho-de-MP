2523f52da0c869fcb9a5ad9828d8c87f9450f8e4
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-05 17:14:01 -0300

    adicionando validacao intervalo point_b

--- Api/app/models/game.rb
+++ Api/app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ApplicationRecord
     validates :point_a, :point_b, presence: true
-    validates :point_a, numericality: {less_than_or_equal_to: 12, greater_than_or_equal_to: 0}
+    validates :point_a, :point_b ,numericality: {less_than_or_equal_to: 12, greater_than_or_equal_to: 0}
 end
