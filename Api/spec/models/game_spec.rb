08cefe09b05767934a8e55dc990db78685608138
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-05 16:52:48 -0300

    configurando factory_bot e primeiro teste

--- Api/spec/models/game_spec.rb
+++ Api/spec/models/game_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory' do
+    it { expect(build(:game)).to be_valid }
+  end
 end
