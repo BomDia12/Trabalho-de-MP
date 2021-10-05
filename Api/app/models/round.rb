9c64d9bf3f5d1201a9cb1278edeb851b2ec6e5d8
Nícolas <nicpabe@gmail.com>
2021-10-05 20:05:52 -0300

    passando teste de multiplicador inválido

--- Api/app/models/round.rb
+++ Api/app/models/round.rb
@@ -2,4 +2,5 @@ class Round < ApplicationRecord
   belongs_to :game
   validates :points_a, :points_b, :multiplier, :turn, presence: true
   validates :points_a, :points_b, numericality: { less_than: 3, greater_than_or_equal_to: 0 }
+  validates :multiplier, inclusion: { in: [1, 3, 6, 9, 12] }
 end
