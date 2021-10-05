7993127d2863244580da92632c4f771ebdf12b96
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-05 16:55:29 -0300

    adicionando caso teste quando point_a e nil

--- Api/spec/models/game_spec.rb
+++ Api/spec/models/game_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Game, type: :model do
   describe 'factory' do
     it { expect(build(:game)).to be_valid }
   end
+
+  describe 'validations' do
+    context 'when has an empty field' do
+      it { expect(build(:game, point_a: nil)).to be_invalid }
+    end
+  end
 end
