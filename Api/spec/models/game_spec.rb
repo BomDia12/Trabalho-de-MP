6eccf93b108294ac39f984e82e6ee0dfd1b2855d
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-05 17:09:06 -0300

    novos testes validacao intervalo point_a

--- Api/spec/models/game_spec.rb
+++ Api/spec/models/game_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe Game, type: :model do
 
     context 'when points are invalid' do
       it { expect(build(:game, point_a: -1)).to be_invalid }
+      it { expect(build(:game, point_a: 13)).to be_invalid }
+      it { expect(build(:game, point_a: 12)).to be_valid }
+      it { expect(build(:game, point_a: 0)).to be_valid }
     end
   end
 end
