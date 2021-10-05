7538ee999594f1a8289788fef8001795809221f3
Nícolas <nicpabe@gmail.com>
2021-10-05 19:48:46 -0300

    teste de pontob inválido

--- Api/app/models/round.rb
+++ Api/app/models/round.rb
@@ -1,5 +1,5 @@
 class Round < ApplicationRecord
   belongs_to :game
   validates :points_a, :points_b, :multiplier, :turn, presence: true
-  validates :points_a, numericality: { less_than: 3, greater_than_or_equal_to: 0 }
+  validates :points_a, :points_b, numericality: { less_than: 3, greater_than_or_equal_to: 0 }
 end
