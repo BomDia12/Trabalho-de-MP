a9b389e0d17b56ab70dde3cf70933bed37a794fc
Nícolas <nicpabe@gmail.com>
2021-10-05 17:33:43 -0300

    teste pontos b nulo

--- Api/spec/models/round_spec.rb
+++ Api/spec/models/round_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Round, type: :model do
     context "when has an empty field" do
       it { expect(build(:round, game_id: nil)).to be_invalid }
       it { expect(build(:round, points_a: nil)).to be_invalid }
+      it { expect(build(:round, points_b: nil)).to be_invalid }
     end
   end
 end
