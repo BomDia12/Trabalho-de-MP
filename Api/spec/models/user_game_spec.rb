6a0735616b30bfdd915db6ed6943b81d4d5e683d
BomDia12 <chamatico2@gmail.com>
2021-10-06 17:07:56 -0300

    :card_file_box: UserGame not null
    Reverting changes that allowed user id value to be null

--- Api/spec/models/user_game_spec.rb
+++ Api/spec/models/user_game_spec.rb
@@ -7,12 +7,6 @@ RSpec.describe UserGame, type: :model do
     end
   end
 
-  describe 'Anonymous User' do
-    context 'should allow for no user' do
-      it { expect(build(:user_game, user_id: nil)).to be_valid }
-    end
-  end
-
   describe 'validations' do
     context "shouldn't allow numbers outside 0..3" do
       it { expect(build(:user_game, player: -1)).to be_invalid }
