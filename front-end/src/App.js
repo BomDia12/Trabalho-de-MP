18d4caa02f9120d935f79187c5aa6910567a9047
nandosts <fernando.jsts@gmail.com>
2021-10-06 12:41:12 -0300

    Initializing routes and styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import Routes from './Routes';
+import { GlobalStyle } from "./styles/Global";
+
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
