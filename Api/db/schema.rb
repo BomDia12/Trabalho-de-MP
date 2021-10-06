126a38d580057a2baa7be13a3c41a470919c39fa
BomDia12 <chamatico2@gmail.com>
2021-10-06 14:08:20 -0300

    :card_file_box: Player
    Adding player to UserGame model

--- Api/db/schema.rb
+++ Api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_165920) do
+ActiveRecord::Schema.define(version: 2021_10_06_170727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_10_06_165920) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "player"
     t.index ["user_id"], name: "index_user_games_on_user_id"
   end
 
