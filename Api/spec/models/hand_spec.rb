9b90b03a754a11a0df0570cf77422d43c76f027e
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-06 09:04:05 -0300

    refatorando

--- Api/spec/models/hand_spec.rb
+++ Api/spec/models/hand_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Hand, type: :model do
   describe 'factory' do
-    context 'when using classic factory_bot'
-    it { expect(build(:hand)).to be_valid }
+    context 'when using classic factory_bot' do
+      it { expect(build(:hand)).to be_valid }
+    end
   end
+
 end
