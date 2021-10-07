043834658f4f53e7d983579277422b03b4cd4ef7
nandosts <fernando.jsts@gmail.com>
2021-10-07 10:24:56 -0300

    :art: Wrap content in tags
    Organizando a parte de regras em tags de html, para facilitar a organizaçao e compreençao dos funcionamentos gerais

--- front-end/src/pages/GameRules/styles.js
+++ front-end/src/pages/GameRules/styles.js
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.h1`
+export const Container = styled.div`
+padding: 2vh 10vw;
 
+width: 100%;
+
+  div {
+    margin-bottom: 5vh;
+  }
+
+  p, li, th, td {
+    font-size: 1.1rem;
+    line-height: 1.4rem;
+  }
+  
+  ul {
+    padding-top: .5rem;
+    margin-left: 3rem;
+  }
 `;
