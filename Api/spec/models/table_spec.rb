96fa6efa21ff9d054f73bd23e7085f621d9e7e83
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-07 17:53:02 -0300

    mesa com cartas sem naipe

--- Api/spec/models/table_spec.rb
+++ Api/spec/models/table_spec.rb
@@ -17,5 +17,12 @@ RSpec.describe Table, type: :model do
     context 'should be referencing a round' do
       it { expect(build(:table, round_id: nil)).to be_invalid }
     end
+
+    context 'when card has no suit' do
+      it { expect(build(:table, card_a: "naipe 3")).to be_invalid }
+      it { expect(build(:table, card_b: "naipe 2")).to be_invalid }
+      it { expect(build(:table, card_c: "naipe 7")).to be_invalid }
+      it { expect(build(:table, card_d: "naipe 7")).to be_invalid }
+    end
   end
 end
