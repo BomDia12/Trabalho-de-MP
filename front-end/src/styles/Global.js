9dcaef55900fbcb0fb595b9f89a510831f32915a
nandosts <fernando.jsts@gmail.com>
2021-10-18 17:10:11 -0300

    :bento: Adicionando variveis de cores
    Adicionei no global.js as variveis de cores. Devem ser usadas no meio do styled components como "var(--nome da variavel)"

--- front-end/src/styles/Global.js
+++ front-end/src/styles/Global.js
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        --army-green: #2B4614;
+        --black: #000000;
+        --bright-yellow-crayola: #FBB13C;
+        --international-orange: #BA1200;
+        --prussian-blue: #1D3557;
+    }
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
-`
+`;
