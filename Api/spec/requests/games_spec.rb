7c70d138db30b97f45f1799e2a9457754d1859ae
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-18 10:30:18 -0300

    teste create_games

--- Api/spec/requests/games_spec.rb
+++ Api/spec/requests/games_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Games", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST create" do
+    context "with valid params" do
+      before do
+        post '/games/new'
+      end
+      it 'return a success response' do
+        expect(response).to have_http_status(:created)
+      end
+    end
   end
 end
