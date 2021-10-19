0d169b2230d05f6a9b7ee0965caa5d1b8b386550
BomDia12 <chamatico2@gmail.com>
2021-10-19 20:14:20 -0300

    ✅ Adding redundent tests
    adding redundent play tests for all the card and turn possibilities, to ensure nothing breaks

--- Api/spec/requests/games_spec.rb
+++ Api/spec/requests/games_spec.rb
@@ -63,5 +63,107 @@ RSpec.describe 'Games', type: :request do
         expect(round.tables.last.card_a).to eql '♠ 3'
       end
     end
+
+    context 'sending card b and turn 1' do
+      let(:game) { create(:game) }
+      let(:round) { create(:round, game_id: game.id, turn: 1) }
+      let(:hand) { create(:hand, round_id: round.id) }
+      before do
+        create(:table,
+               round_id: round.id,
+               card_a: nil,
+               card_b: nil,
+               card_c: nil,
+               card_d: nil)
+        post '/games/play', params: {
+          hand_id: hand.id,
+          card: 'b'
+        }
+      end
+
+      it 'should return ok code' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'responds with a json' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'removes card from hand' do
+        expect(Hand.find(hand.id).card_b).to be_nil
+      end
+
+      it 'places right card on table' do
+        expect(round.tables.last.card_b).to eql '♥ 2'
+      end
+    end
+
+    context 'sending card c and turn 2' do
+      let(:game) { create(:game) }
+      let(:round) { create(:round, game_id: game.id, turn: 2) }
+      let(:hand) { create(:hand, round_id: round.id) }
+      before do
+        create(:table,
+               round_id: round.id,
+               card_a: nil,
+               card_b: nil,
+               card_c: nil,
+               card_d: nil)
+        post '/games/play', params: {
+          hand_id: hand.id,
+          card: 'c'
+        }
+      end
+
+      it 'should return ok code' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'responds with a json' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'removes card from hand' do
+        expect(Hand.find(hand.id).card_c).to be_nil
+      end
+
+      it 'places right card on table' do
+        expect(round.tables.last.card_c).to eql '♦ 2'
+      end
+    end
+
+    context 'sending card c and turn 3' do
+      let(:game) { create(:game) }
+      let(:round) { create(:round, game_id: game.id, turn: 3) }
+      let(:hand) { create(:hand, round_id: round.id) }
+      before do
+        create(:table,
+               round_id: round.id,
+               card_a: nil,
+               card_b: nil,
+               card_c: nil,
+               card_d: nil)
+        post '/games/play', params: {
+          hand_id: hand.id,
+          card: 'c'
+        }
+      end
+
+      it 'should return ok code' do
+        expect(response).to have_http_status :ok
+      end
+
+      it 'responds with a json' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'removes card from hand' do
+        expect(Hand.find(hand.id).card_c).to be_nil
+      end
+
+      it 'places right card on table' do
+        expect(round.tables.last.card_d).to eql '♦ 2'
+      end
+    end
   end
 end
