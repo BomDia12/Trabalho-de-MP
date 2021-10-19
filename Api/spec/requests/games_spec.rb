f764dcfbbc8340bc043408d785e3dabe92f944ea
Leir-Cruz <gabrielcruzvazsantos@gmail.com>
2021-10-19 14:58:51 -0300

    refatorando e adicionando novos casos testes

--- Api/spec/requests/games_spec.rb
+++ Api/spec/requests/games_spec.rb
@@ -17,24 +17,50 @@ RSpec.describe "Games", type: :request do
 
   describe "GET construct_hands" do
 
-    let(:roundbatata) { create :round }
+    let(:round) { create :round }
     before do
-      @hand1 = create(:hand, round_id: roundbatata.id)
-      @hand2 = create(:hand, round_id: roundbatata.id)
-      @hand3 = create(:hand, round_id: roundbatata.id)
-      @hand4 = create(:hand, round_id: roundbatata.id)
-      roundbatata.reload
+      @hand1 = create(:hand, round_id: round.id)
+      @hand2 = create(:hand, round_id: round.id)
+      @hand3 = create(:hand, round_id: round.id)
+      @hand4 = create(:hand, round_id: round.id)
+      round.reload
     end
     
+    
+    context "when players hand belongs to an round" do
+      before { get "/games/round_begins/#{round.id}" }
+      it { expect(round.id).to eq(@hand1.round_id) }
+      it { expect(round.hands[0].id).to eq(@hand1.id) }
+      it { expect(round.hands[1].id).to eq(@hand2.id) }
+      it { expect(round.hands[2].id).to eq(@hand3.id) }
+      it { expect(round.hands[3].id).to eq(@hand4.id) }
+      it "should return an ok status " do
+        expect(response).to have_http_status(:ok)
+      end
+      it "should response with an json" do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
 
-    context "when the players have cards" do
-      before { get "/games/round_begins/#{roundbatata.id}" }
-      it { expect(roundbatata.id).to eq(@hand1.round_id) }
-      it { expect(roundbatata.hands[0].id).to eq(@hand1.id) }
-      it { expect(roundbatata.hands[1].id).to eq(@hand2.id) }
-      it { expect(roundbatata.hands[2].id).to eq(@hand3.id) }
-      it { expect(roundbatata.hands[3].id).to eq(@hand4.id) }
-      it { expect(response).to have_http_status(:ok) }
+    context "when there is less than 4 players in an round" do
+      before do
+        @hand4.destroy!
+        round.reload
+      end
+      before { get "/games/round_begins/#{round.id}" }
+      it "should return an bad_request status" do
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context "when there is more than 4 players in an round" do
+      before do
+      @hand5 = create(:hand, round_id: round.id)
+      end
+      before { get "/games/round_begins/#{round.id}" }
+      it "should return an bad_request status" do
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 end
