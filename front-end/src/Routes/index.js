98a210ed4473573094debcd231c31b6d2658261e
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-10-19 22:23:46 -0300

    feat(resgister): Inicializa escopo da pagina de cadastro

--- front-end/src/Routes/index.js
+++ front-end/src/Routes/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 import HomePage from '../pages/HomePage'
 import Rules from '../pages/GameRules'
+import Register from '../pages/Register'
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/rules' component={Rules} />
+      <Route exact path='/register' component={Register} />
     </Switch>
   )
 }
---
+++ front-end/src/pages/Register/index.js
@@ -0,0 +1,10 @@
+// Arquivo da página inicial
+import { Container } from "./styles"
+
+const Register = () => {
+    return (
+        <Container>Cadastro</Container>
+    )
+}
+
+export default Register
