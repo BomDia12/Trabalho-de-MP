b9d974ff216f47c070a9c083637379fae4c3081a
BomDia12 <chamatico2@gmail.com>
2021-10-19 21:49:00 -0300

    🐛 Turn not null
    not allowing turn to be nil

--- Api/app/models/round.rb
+++ Api/app/models/round.rb
@@ -22,7 +22,8 @@ class Round < ApplicationRecord
   validates :points_a, :points_b, :multiplier, :turn, presence: true
   validates :points_a, :points_b, numericality: { less_than: 3, greater_than_or_equal_to: 0 }
   validates :multiplier, inclusion: { in: [1, 3, 6, 9, 12] }
-  validates :multiplier_turn, :turn, inclusion: [*0..3, nil]
+  validates :multiplier_turn, inclusion: [*0..3, nil]
+  validates :turn, inclusion: 0..3
   validates :ended, inclusion: [true, false], exclusion: [nil]
   validates_with ValidateEnded
   has_many :hands
