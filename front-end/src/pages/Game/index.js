fcb81b3f9c1136f37a992d37533ba12138faf088
nandosts <fernando.jsts@gmail.com>
2021-10-21 20:52:00 -0300

    :bento: Add cards to table
    Adding cards to the table, finishing the first prototype non integrated

--- front-end/src/pages/Game/index.js
+++ front-end/src/pages/Game/index.js
@@ -1,13 +1,40 @@
-import { Container, Table } from "./styles";
+import {
+  BottomPlayer,
+  Container,
+  LeftPlayer,
+  RightPlayer,
+  Table,
+  TopPlayer,
+} from "./styles";
 import CardBack from "../../components/GameCardBack";
 
 const Game = () => {
   return (
     <Container>
-      <Table>
-        Truco
+      <Table>Truco</Table>
+      <TopPlayer>
         <CardBack />
-      </Table>
+        <CardBack />
+        <CardBack />
+      </TopPlayer>
+
+      <RightPlayer>
+        <CardBack />
+        <CardBack />
+        <CardBack />
+      </RightPlayer>
+
+      <BottomPlayer>
+        <CardBack />
+        <CardBack />
+        <CardBack />
+      </BottomPlayer>
+
+      <LeftPlayer>
+        <CardBack />
+        <CardBack />
+        <CardBack />
+      </LeftPlayer>
     </Container>
   );
 };
--- front-end/src/pages/Game/styles.js
+++ front-end/src/pages/Game/styles.js
@@ -11,6 +11,7 @@ export const Container = styled.div`
 `;
 
 export const Table = styled.div`
+  position: relative;
   background: radial-gradient(
     50% 50% at 50% 50%,
     #406c1b 0%,
@@ -27,4 +28,31 @@ export const Table = styled.div`
   font-family: "Cinzel", serif;
   font-size: 4rem;
   color: white;
+`;
+
+export const TopPlayer = styled.div`
+  position: absolute;
+  top: 10px;
+  display: flex;
+  transform: rotate(180deg);
+`;
+
+export const RightPlayer = styled.div`
+  position: absolute;
+  right: -50px;
+  display: flex;
+  transform: rotate(-90deg);
+`;
+
+export const BottomPlayer = styled.div`
+  position: absolute;
+  bottom: 10px;
+  display: flex;
+`;
+
+export const LeftPlayer = styled.div`
+  position: absolute;
+  left: -50px;
+  display: flex;
+  transform: rotate(90deg);
 `;
