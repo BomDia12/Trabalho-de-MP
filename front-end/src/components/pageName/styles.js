3f243e40df9e413a28d59c9078f89acd61770d05
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-10-22 18:42:54 -0300

    feat(pagename): cria componente de titulo da pagina

---
+++ front-end/src/components/pageName/styles.js
@@ -0,0 +1,26 @@
+// Arquivo de estilos do componente de titulo da pagina
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #2B4614;
+    height: 101px;
+    width: 703px;
+    display: flex;
+    /* /* align-items: center; */
+    /* width: 50%; */
+    /* height: 100%; */
+    justify-content: flex-start;
+    align-self: left;
+    margin-top: 30px;
+`;
+
+export const Title = styled.h1`
+    align-self: left;
+    color: #ffffff;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 64px;
+    line-height: 86px;
+    margin-left: 4rem;
+`;
