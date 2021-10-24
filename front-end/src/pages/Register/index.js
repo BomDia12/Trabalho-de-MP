5dd5885ebc02e7e0da1f7ffe2b954c8b2c347664
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-10-24 16:46:40 -0300

    feat(register): monta estrutura da pagina de cadastro

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,9 +1,12 @@
 // Arquivo da página inicial
-import { RegisterContainer } from "./styles"
+import { RegisterContainer, Box } from "./styles"
 
 import PageName from '../../components/pageName'
 import Header from '../../components/header'
+import Input from '../../components/input'
+// import Button from "../../components/button"
 import arrow from '../../assets/arrowBack/arrow.png'
+import naipe2 from '../../assets/naipes/naipe2.png'
 
 
 const Register = () => {
@@ -13,6 +16,43 @@ const Register = () => {
             <PageName 
                 title= "Cadastro"
             />
+            <Box> 
+                <span>{7}</span>
+                <img className="top-card" src={naipe2} alt="Naipe da carta"/>
+                <Input 
+                    label={"Nome"} 
+                    name={"username"} 
+                    type={"text"}
+                    id={"name"} />
+                <Input 
+                    label={"Email"} 
+                    name={"email"} 
+                    type={"text"}
+                    id={"email"} />
+                <Input 
+                    label={"Senha"} 
+                    name={"passwprd"} 
+                    type={"password"}
+                    id={"password"} />
+                <Input 
+                    label={"Confirme sua senha"} 
+                    name={"passwprd_confirmation"} 
+                    type={"password"}
+                    id={"password_confirmation"} />
+
+                {/* <Button/> */}
+                <input class="button" type="button" id="login" value="entrar"/>
+
+                <div>
+          <         span>Já possui conta?</span><span className="span-click"> Faça login</span>
+                </div>
+
+                <div className="div-naipe">
+                    <img className="botton-card" src={naipe2} alt="Naipe da carta"/>
+                </div>
+                <span>{7}</span>
+
+            </Box> 
         </RegisterContainer>
     )
 }
--- front-end/src/pages/Register/styles.js
+++ front-end/src/pages/Register/styles.js
@@ -1,4 +1,3 @@
-// Arquivo de estilos da página inicial
 
 import styled from 'styled-components';
 
@@ -9,4 +8,54 @@ export const RegisterContainer = styled.div`
     justify-content: flex-start;
     flex-direction: column;
 
+`;
+
+export const Box = styled.div`
+    background-color: #ffffff;
+    width: 500px;
+    height: 600px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    align-self: center;
+    flex-direction: column;
+    border-radius: 8px;
+
+    .top-card {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            justify-content: flex-start;
+
+            img {
+                width: 1.5rem;
+            }
+        }
+
+    .bottom-card {
+        display: flex;
+        justify-content: flex-end;
+        
+        .div-naipe {
+            bottom: 0;
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+        }
+
+        img {
+            width: 1.5rem;
+        }
+    }
+    .button {
+        background-color: #2B4614;
+        color: #ffffff;
+        width: 5rem;
+        height: 2rem;
+        border-radius: 30px;
+    }
+    .span-click {
+        color: #2B4614;
+        font-weight: 600;
+}
 `;
