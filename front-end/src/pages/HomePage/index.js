90710c14e3c5668c93c1513057b8394798267671
nandosts <fernando.jsts@gmail.com>
2021-10-24 20:50:25 -0300

    :rocket: Integrate start game on homepage
    Put the start game function on homepage, on the "Jogue Aqui" button

--- front-end/src/pages/HomePage/index.js
+++ front-end/src/pages/HomePage/index.js
@@ -1,50 +1,77 @@
 // Arquivo da página inicial
-import { FirstContainer, SecondContainer, TitleContainer } from './styles';
-import Title from '../../components/Title'
-import CardsImage from '../../assets/images/Rectangle.png'
-import Tree from '../../assets/images/tree.png'
-import Group from '../../assets/images/group.png'
-import Ai from '../../assets/images/ai.png'
-import Wagon from '../../assets/images/wagon.png'
+import { FirstContainer, SecondContainer, TitleContainer } from "./styles";
+import Title from "../../components/Title";
+import CardsImage from "../../assets/images/Rectangle.png";
+import Tree from "../../assets/images/tree.png";
+import Group from "../../assets/images/group.png";
+import Ai from "../../assets/images/ai.png";
+import Wagon from "../../assets/images/wagon.png";
 
-import Button from '../../components/Button'
+import { api } from "../../services/api";
+import { useHistory } from "react-router-dom";
+import { useUserContext } from "../../context/user";
+
+import Button from "../../components/Button";
 
 const HomePage = () => {
-    return (
-        <>
-        <FirstContainer>
-            <img src={CardsImage}></img>
-            <TitleContainer>
-                    <Title>O FUTURO DO TRUCO</Title>
-            </TitleContainer>
-            <Button>Jogue Agora</Button>
-        </FirstContainer>
+  const { user } = useUserContext;
+  const history = useHistory();
 
-        <SecondContainer>
-            <div className='content-wrapper'>
-                <Title><span>Orientado a Testes</span></Title>
-                <img src={Tree}></img>
-                <p>Feito sob protocolo para uma melhor experiência</p>
-            </div>
-            <div className='content-wrapper-2'>
-                <Title><span>Modo Multijogador</span></Title>
-                <img src={Group}></img>
-                <p>Jogue com seus amigos e divirta-se</p>
-            </div>
-            <div className='content-wrapper'>
-                <Title><span>Truco Mineiro</span></Title>
-                <img src={Wagon}></img>
-                <p>Jogue a modalidade de truco queridinha do Brasil</p>
-            </div>
-            <div className='content-wrapper-2'>
-                <Title><span>Inteligência Artificial</span></Title>
-                <img src={Ai}></img>
-                <p>Jogue contra o computador e teste suas habilidades</p>
-            </div>
+  const StartGame = (e) => {
+    e.preventDefault();
+    api
+      .post("games/new", {
+        users: [user ? user.id : ""],
+      })
+      .then((response) => {
+        response.status === 201
+          ? alert("Criado com sucesso")
+          : alert("Erro, tente novamente");
+        history.push(`/game/${response.data.id}/0`);
+      });
+  };
+  return (
+    <>
+      <FirstContainer>
+        <img src={CardsImage}></img>
+        <TitleContainer>
+          <Title>O FUTURO DO TRUCO</Title>
+        </TitleContainer>
+        <Button onClick={StartGame}>Jogue Agora</Button>
+      </FirstContainer>
 
-        </SecondContainer>
-        </>
-    );
-}
+      <SecondContainer>
+        <div className="content-wrapper">
+          <Title>
+            <span>Orientado a Testes</span>
+          </Title>
+          <img src={Tree}></img>
+          <p>Feito sob protocolo para uma melhor experiência</p>
+        </div>
+        <div className="content-wrapper-2">
+          <Title>
+            <span>Modo Multijogador</span>
+          </Title>
+          <img src={Group}></img>
+          <p>Jogue com seus amigos e divirta-se</p>
+        </div>
+        <div className="content-wrapper">
+          <Title>
+            <span>Truco Mineiro</span>
+          </Title>
+          <img src={Wagon}></img>
+          <p>Jogue a modalidade de truco queridinha do Brasil</p>
+        </div>
+        <div className="content-wrapper-2">
+          <Title>
+            <span>Inteligência Artificial</span>
+          </Title>
+          <img src={Ai}></img>
+          <p>Jogue contra o computador e teste suas habilidades</p>
+        </div>
+      </SecondContainer>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
