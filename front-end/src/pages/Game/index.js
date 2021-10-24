20c31e1b0abec7b977b8ae0b490604f6674f779c
manigoldTLC <gabrxavier353@gmail.com>
2021-10-24 18:45:15 -0300

    :sparkles: mostrando pontos dos jogadores

--- front-end/src/pages/Game/index.js
+++ front-end/src/pages/Game/index.js
@@ -8,14 +8,40 @@ import {
 } from "./styles";
 import CardBack from "../../components/GameCardBack";
 import { useParams } from "react-router";
+import { api } from "../../services/api";
+import { useState, useEffect } from 'react'
 
 const Game = () => {
 
-    const { player, game_id } = useParams()
+  const [ round, setRound ] = useState([]);
+
+  const { player, game_id } = useParams()
+
+  const getRound = async () => {
+    const response = await api.get(`/games/${game_id}`)
+
+    setRound(response.data.rounds.filter(round => !round.ended))
+    
+  }
+  
+  useEffect(() => {
+    getRound();
+  }, []);
+  
+  const showRound = (round) => {
+    let pointsA, pointsB;
+
+    for (let i = 0; i < round.length; i ++){
+      pointsA = round[i].points_a
+      pointsB = round[i].points_b
+    }
+
+    return `pontos A: ${pointsA}, pontos B: ${pointsB}`
+  }
 
   return (
     <Container>
-      <Table>Truco</Table>
+      <Table>Truco {showRound(round)}</Table>
       <TopPlayer>
         <CardBack />
         <CardBack />
