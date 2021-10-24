0de07999ff9aa80f3e5cd18b536030f7b370aec9
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-10-24 18:12:08 -0300

    feat(routes): adiciona rota para pagina de login

--- front-end/src/Routes/index.js
+++ front-end/src/Routes/index.js
@@ -4,6 +4,7 @@ import HomePage from '../pages/HomePage'
 import UserProfile from '../pages/UserProfile'
 import Rules from '../pages/GameRules'
 import Register from '../pages/Register'
+import Login from '../pages/Login'
 
 const Routes = () => {
   return (
@@ -12,6 +13,7 @@ const Routes = () => {
       <Route path='/user_perfil' component={UserProfile} />
       <Route exact path='/rules' component={Rules} />
       <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
     </Switch>
   )
 }
