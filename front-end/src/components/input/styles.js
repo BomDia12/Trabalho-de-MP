f57f025a4a0cba7d125de4637733252c144dcf6e
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-10-24 16:45:50 -0300

    feat(input): cria componente de input

---
+++ front-end/src/components/input/styles.js
@@ -0,0 +1,13 @@
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #FFFFFF;
+    width: 350px;
+    height: 50px;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-self: center;
+    
+`;
